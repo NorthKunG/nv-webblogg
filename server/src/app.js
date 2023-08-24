44b5d7564add3d3013d1492c31ecb25fc2f10b87
NorthKunG <112927808+NorthKunG@users.noreply.github.com>
2023-08-24 16:33:23 +0700

    folder perpared

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,42 @@
-console.log('Hello nodejs server')
+let express = require("express")
+let bodyParser = require("body-parser")
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get("/status", function(req, res) {
+    res.send("Hello nodejs server")
+})
+
+app.get("/hello/:person", function(req, res) {
+    console.log("hello - " + req.params.person)
+    res.send("say hello with " + req.params.person)
+})
+
+app.get("/user/:userID", function(req, res) {
+    res.send("ดูข้อมูลผู้ใช้งาน")
+})
+
+app.get("/users", function(req, res) {
+    res.send("เรียกข้อมูล ผู้ใช้งานทั้งหมด")
+})
+
+app.post("/user/", function(req, res) {
+    res.send("ทำการสร้างผู้ใช้งาน: " + JSON.stringify(req.body))
+})
+
+app.put("/user/:userID", function(req, res) {
+    res.send("ทำการแก้ไขผู้ใช้งาน: " + req.params.userID + ": " + JSON.stringify(req.body))
+})
+
+app.delete("/user/:userID", function(req, res) {
+    res.send("ทำการลบผู้ใช้งาน: " + req.params.userID + ": " + JSON.stringify(req.body))
+})
+
+let port = 8081
+
+app.listen(port, function() {
+    console.log("server running on " + port)
+})
